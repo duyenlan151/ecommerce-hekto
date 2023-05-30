043dcf5bc9cfbfce0e8424215af4d5e9099f5fc3
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-05-30 18:42:01 +0700

    [UPD] update base url

--- services/api-services.ts
+++ services/api-services.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosError } from 'axios';
 
 const axiosClient = axios.create({
-  // baseURL: process.env.BASE_URL,
-  baseURL: 'http://localhost:3000/api/admin/',
+  baseURL: process.env.BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
