58d8c69636b4a0e5025e129a73d1feca9fe20c92
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-05-30 22:55:32 +0700

    [UPD] update

--- pages/api/admin/categories.ts
+++ pages/api/admin/categories.ts
@@ -4,66 +4,62 @@ import { getServerSession } from 'next-auth';
 // import { authOptions, isAdminRequest } from "@/pages/api/auth/[...nextauth]";
 
 export default async function handle(req, res) {
-  try {
-    const { method, query } = req;
-    const rest = await mongooseConnect();
-    // await isAdminRequest(req, res);
+  const { method, query } = req;
+  await mongooseConnect();
+  // await isAdminRequest(req, res);
 
-    switch (method) {
-      case 'GET': {
-        if (req.query?.id) {
-          res.status(200).json(await Category.findOne({ _id: query.id }));
-        } else {
-          const data = await Category.find();
-          res.status(200).json({
-            data,
-            total: data.length,
-            linmit: 10,
-            page: 1,
-            message: 'Find all success!',
-            statusCode: 200,
-          });
-        }
-        break;
-      }
-      case 'POST': {
-        const { name, parentCategory, properties, slug, status } = req.body;
-        console.log('🚀 ~ file: categories.ts:30 ~ handle ~ status:', status);
-        const categoryDoc = await Category.create({
-          name,
-          properties,
-          slug,
-          status,
+  switch (method) {
+    case 'GET': {
+      if (req.query?.id) {
+        res.status(200).json(await Category.findOne({ _id: query.id }));
+      } else {
+        const data = await Category.find();
+        res.status(200).json({
+          data,
+          total: data.length,
+          linmit: 10,
+          page: 1,
+          message: 'Find all success!',
+          statusCode: 200,
         });
-        res.status(200).json(categoryDoc);
-        break;
       }
-      case 'PUT':
-        {
-          const { name, parentCategory, properties, _id, slug, status } = req.body;
-          const categoryDoc = await Category.updateOne(
-            { _id },
-            {
-              name,
-              slug,
-              status,
-              // parent: parentCategory || undefined,
-            }
-          );
-          res.status(200).json(categoryDoc);
-        }
-        break;
-      case 'DELETE': {
-        const { _id } = req.query;
-        await Category.deleteOne({ _id });
-        res.status(200).json('ok');
-      }
-
-      default:
-        break;
+      break;
     }
-    res.status(200).json({ categories: 'categories' });
-  } catch (error) {
-    res.status(200).json({ error });
+    case 'POST': {
+      const { name, parentCategory, properties, slug, status } = req.body;
+      console.log('🚀 ~ file: categories.ts:30 ~ handle ~ status:', status);
+      const categoryDoc = await Category.create({
+        name,
+        properties,
+        slug,
+        status,
+      });
+      res.status(200).json(categoryDoc);
+      break;
+    }
+    case 'PUT':
+      {
+        const { name, parentCategory, properties, _id, slug, status } = req.body;
+        const categoryDoc = await Category.updateOne(
+          { _id },
+          {
+            name,
+            slug,
+            status,
+            // parent: parentCategory || undefined,
+          }
+        );
+        res.status(200).json(categoryDoc);
+      }
+      break;
+    case 'DELETE': {
+      const { _id } = req.query;
+      await Category.deleteOne({ _id });
+      res.status(200).json('ok');
+    }
+
+    default:
+      res.status(200).json({ categories: 'categories' });
+      break;
   }
 }
