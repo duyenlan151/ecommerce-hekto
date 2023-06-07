aaaa11d7fe9480eeefa38e641fa35da4892a9321
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-07 14:16:30 +0700

    [UPD] uddate style sidebar admin

--- components/Shared/Sidebar.tsx
+++ components/Shared/Sidebar.tsx
@@ -5,6 +5,7 @@ import { useRouter } from 'next/router';
 import { useState } from 'react';
 import {
   AiOutlineAppstore,
+  AiOutlineClose,
   AiOutlineHome,
   AiOutlineLogout,
   AiOutlineMenu,
@@ -35,8 +36,8 @@ const items = [
 ];
 
 export default function Sidebar() {
-  const [collapseShow, setCollapseShow] = useState('hidden');
   const router = useRouter();
+  const [show, setShow] = useState(false);
   const dispatch = useDispatch();
 
   const handleLogout = () => {
@@ -44,22 +45,22 @@ export default function Sidebar() {
     signOut({ callbackUrl: '/' });
   };
   return (
-    <div className="font-lato md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10">
-      <div className="md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-between w-full mx-auto">
+    <div className="font-lato relative md:left-0 md:block md:fixed md:top-0 md:bottom-0 md:overflow-y-auto md:flex-row md:flex-nowrap md:overflow-hidden shadow-xl bg-white flex flex-wrap items-center justify-between relative md:w-64 z-10">
+      <div className="w-full relative md:flex-col md:items-stretch md:min-h-full md:flex-nowrap px-0 flex flex-wrap items-center justify-center w-full mx-auto">
         {/* Toggler */}
         <button
-          className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
+          className="cursor-pointer absolute left-0 text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
           type="button"
-          onClick={() => setCollapseShow('bg-white m-2 py-3 px-6')}
+          onClick={() => setShow(true)}
         >
-          <i className="fas fa-bars"></i>
+          <AiOutlineMenu />
         </button>
         {/* Brand */}
         <Link
           href={`/admin/dashboard`}
           className="md:block text-center text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-2"
         >
-          Notus NextJS
+          Hekto | Ecommerce
         </Link>
         {/* User */}
         <ul className="md:hidden items-center flex flex-wrap list-none">
@@ -68,15 +69,14 @@ export default function Sidebar() {
         </ul>
         {/* Collapse */}
         <div
-          className={
-            'md:flex md:flex-col md:items-stretch md:opacity-100 md:relative lg:mt-4 md:shadow-none shadow absolute top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden h-auto items-center flex-1 rounded ' +
-            collapseShow
-          }
+          className={`max-w-[380px] w-[90%] bg-white min-h-screen md:z-50 fixed top-0 transition-transform duration-300 ease-out md:flex md:flex-col md:items-stretch md:opacity-100 md:relative lg:mt-4 md:shadow-none shadow top-0 left-0 right-0 z-40 overflow-y-auto overflow-x-hidden items-center flex-1 rounded ${
+            show ? 'translate-x-0' : 'md:-translate-x-0 -translate-x-full'
+          }`}
         >
           {/* Collapse header */}
-          <div className="md:min-w-full md:hidden block pb-4 mb-4 border-b border-solid border-blueGray-200">
+          <div className="md:min-w-full md:hidden ">
             <div className="flex flex-wrap">
-              <div className="w-6/12">
+              <div className="w-6/12 px-4">
                 <Link
                   href="#pablo"
                   className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
@@ -88,18 +88,18 @@ export default function Sidebar() {
                 <button
                   type="button"
                   className="cursor-pointer text-black opacity-50 md:hidden px-3 py-1 text-xl leading-none bg-transparent rounded border border-solid border-transparent"
-                  onClick={() => setCollapseShow('hidden')}
+                  onClick={() => setShow(false)}
                 >
-                  <i className="fas fa-times"></i>
+                  <AiOutlineClose />
                 </button>
               </div>
             </div>
           </div>
 
           {/* Divider */}
-          <hr className="md:min-w-full" />
+          <hr className="w-full" />
 
-          <ul className="mt-2 md:flex-col md:min-w-full flex flex-col list-none px-4">
+          <ul className="mt-2 md:flex-col w-full flex flex-col list-none px-4 bg-white">
             {items.map((item) => (
               <Link
                 key={item.id}
@@ -119,6 +119,12 @@ export default function Sidebar() {
           </ul>
         </div>
       </div>
+      {show && (
+        <div
+          onClick={() => setShow(false)}
+          className="md:hidden visible animate-fadeEntering fixed w-screen inset-0 h-screen z-20 bg-black/60 transition-transform ease-in-out duration-500 opacity-100"
+        ></div>
+      )}
     </div>
   );
 }
