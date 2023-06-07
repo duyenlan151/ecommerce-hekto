b0efdb3ba54c227ce2f079ff3f230c2a22087d5d
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-07 15:35:26 +0700

    [UPD]

--- components/Blogs/BlogList.tsx
+++ components/Blogs/BlogList.tsx
@@ -33,9 +33,9 @@ const data = [
 export default function BlogList(props: BlogListProps) {
   return (
     <>
-      <h4 className="text-center text-blue-2 lg:text-5xl md:lg:text-4xl text-3xl font-bold mb-20 tracking-wide">
+      <h5 className="text-center text-blue-2 lg:text-5xl md:lg:text-4xl text-3xl font-bold mb-20 tracking-wide">
         Lasted Blogs
-      </h4>
+      </h5>
       <div className="grid justify-center items-center mx-auto lg:gap-10 md:gap-6 gap-4 lg:grid-cols-3 sm:grid-cols-2 xs:grid-cols-1 grid-flow-row">
         {data.map((blog) => (
           <BlogItem key={blog.id} blog={blog} />
