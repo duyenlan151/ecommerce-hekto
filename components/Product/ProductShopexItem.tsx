f392aa1ddcb828403b42e0beaf89f7e69c1e87e0
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-07 15:28:34 +0700

    [UPD] update text  support

--- components/Product/ProductShopexItem.tsx
+++ components/Product/ProductShopexItem.tsx
@@ -21,7 +21,7 @@ export default function ProductShopexItem({
       className="relative group shadow-lg min-h-[320px] p-5 flex justify-center flex-col items-center text-center"
     >
       <div className="mx-auto">{icon}</div>
-      <h4 className="text-xl text-blue-1 pt-[20px] pb-[15px]">{title}</h4>
+      <p className="text-xl text-blue-1 pt-[20px] pb-[15px]">{title}</p>
       <p className="text-blue-2 opacity-30 font-lato leading-relaxed">{desc}</p>
       <div className="absolute left-0 bottom-0 h-[2px] w-full transform !scale-x-0 group-hover:!scale-x-100 transition-width  origin-center delay-100 ease-in-out duration-500 bg-orange-1"></div>
     </motion.div>
