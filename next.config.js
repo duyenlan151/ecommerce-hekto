0234d359565ba80c30a42ef81b12a474cf4fd2ec
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-09 12:14:53 +0700

    [TES] test api

--- next.config.js
+++ next.config.js
@@ -61,14 +61,12 @@ module.exports = withPWA({
   async headers() {
     return [
       {
-        source: '/',
+        // matching all API routes
+        source: '/api/:path*',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
-          {
-            key: 'Access-Control-Allow-Methods',
-            value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-          },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
             key: 'Access-Control-Allow-Headers',
             value:
