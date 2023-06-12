60584656ae7f56c1b31dc91230d5e43abac78430
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-12 09:58:24 +0700

    [UPD] update page document

--- components/Blogs/BlogSidebar.tsx
+++ components/Blogs/BlogSidebar.tsx
@@ -7,7 +7,7 @@ export interface BlogSidebarProps {}
 
 export function BlogSidebar(props: BlogSidebarProps) {
   return (
-    <aside className="sticky top-0 w-full self-start lg:w-96">
+    <aside className="sticky top-0 w-full self-start xl:96 lg:w-72">
       <div className="mt-5 font-sans">
         <BlogSearch />
         <BlogRelated />
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,9 +10,6 @@ class MyDocument extends Document {
           <meta name="theme-color" content="#000000" />
           <link rel="shortcut icon" href="/img/brand/favicon.ico" />
           <link rel="apple-touch-icon" sizes="76x76" href="/img/brand/apple-icon.png" />
-          {/* <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCRKl1zvLbUPYuOsyYXSZwRtFu9C2EpOYU"></script> */}
-          {/* <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCACwpcek5FdW8bn02P0HG7IA07d1Zd6ys&callback=Function.prototype"></script> */}
-          <script src="https://unpkg.com/react-popper/dist/index.umd.js"></script>
         </Head>
         <body className="text-blueGray-700 antialiased">
           <div id="page-transition"></div>
