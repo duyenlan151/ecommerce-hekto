2bb63d8c5d32c83a0ee08c57333b33a1635c343a
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-12 11:48:36 +0700

    [UPD] update style

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -85,7 +85,7 @@ export default function BlogPage({ blogs }: BlogPageProps) {
       </div>
       <BlogList blogs={dataBlogs} />
       {defaultState.hasNextPage && (
-        <div className="mt-10 flex justify-center">
+        <div className="mt-16 flex justify-center">
           <a
             className="cursor-pointer min-w-[150px] relative inline-flex justify-center items-center gap-1 rounded-md border border-gray-300 bg-white px-3 py-2 pl-4 text-sm font-medium text-gray-500 hover:bg-gray-50 focus:z-20 disabled:pointer-events-none disabled:opacity-40 dark:border-gray-500 dark:bg-gray-800 dark:text-gray-300"
             onClick={handleLoadMore}
