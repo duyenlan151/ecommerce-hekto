2451080bcb8c4c331c57d46fd13a839b9d0f0314
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-14 01:32:30 +0700

    [UPD] update code

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -1,5 +1,6 @@
 import ProductListPage from '@components/Product/ProductListPage';
 import LoadingCommon from '@components/Shared/Common/LoadingCommon';
+import { useRequestWithSWR } from '@hooks/index';
 import { GetServerSideProps } from 'next';
 import { useRouter } from 'next/router';
 import { Suspense } from 'react';
@@ -14,6 +15,14 @@ type Props = {
 
 const ProductsPage = ({ products, limit, total }: Props) => {
   const router = useRouter();
+  const {
+    query: { category, page = 1, price, rating, sort },
+  } = router;
+
+  const { response, isLoading } = useRequestWithSWR({
+    url: '/admin/products',
+    params: { category, page, price, rating, sort },
+  });
 
   if (router.isFallback) {
     return <LoadingCommon />;
@@ -21,46 +30,46 @@ const ProductsPage = ({ products, limit, total }: Props) => {
 
   return (
     <Suspense fallback={<LoadingCommon />}>
-      <ProductListPage products={products} />
+      <ProductListPage products={response} isLoading={isLoading} />
     </Suspense>
   );
 };
 
 export default ProductsPage;
 
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  try {
-    context.res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate=5');
+// export const getServerSideProps: GetServerSideProps = async (context) => {
+//   try {
+// context.res.setHeader('Cache-Control', 's-maxage=5, stale-while-revalidate=5');
 
-    const { req } = context;
-    const cookies = req.headers.cookie;
+// const { req } = context;
+// const cookies = req.headers.cookie;
 
-    axiosClient.defaults.headers.common['Cookie'] = cookies;
+// axiosClient.defaults.headers.common['Cookie'] = cookies;
 
-    const page = context?.query?.page || 1;
-    const price = context?.query?.price || 'all';
-    const rating = context?.query?.rating || 'all';
-    const sort = context?.query?.sort || 'featured';
-    const category = context?.query?.category || '';
+// const page = context?.query?.page || 1;
+// const price = context?.query?.price || 'all';
+// const rating = context?.query?.rating || 'all';
+// const sort = context?.query?.sort || 'featured';
+// const category = context?.query?.category || '';
 
-    const data = await productsService.getAllProducts({
-      page,
-      price,
-      rating,
-      sort,
-      category,
-    });
+// const data = await productsService.getAllProducts({
+//   page,
+//   price,
+//   rating,
+//   sort,
+//   category,
+// });
 
-    // const res = await axiosClient.get(`/admin/products`, {
-    //   withCredentials: true,
-    //   headers: {
-    //     Cookie: `${cookies};`,
-    //   },
-    // });
-    return {
-      props: { products: data },
-    };
-  } catch (error) {
-    return { props: { products: [] } };
-  }
-};
+// const res = await axiosClient.get(`/admin/products`, {
+//   withCredentials: true,
+//   headers: {
+//     Cookie: `${cookies};`,
+//   },
+// });
+//     return {
+//       props: { products: {} },
+//     };
+//   } catch (error) {
+//     return { props: { products: [] } };
+//   }
+// };
