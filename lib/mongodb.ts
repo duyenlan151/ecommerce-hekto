239fa260afd2199a48cba3d05d9748d74615dbce
Loan Duyên <43545283+duyenlan151@users.noreply.github.com>
2023-06-24 23:11:01 +0700

    [UPD] clean code

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -14,18 +14,17 @@ const options = {};
 
 let client;
 let clientPromise;
+const globalAny: any = global;
 
 if (process.env.NODE_ENV === 'development') {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  // @ts-ignore
-  if (!global._mongoClientPromise) {
+
+  if (!globalAny._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    // @ts-ignore
-    global._mongoClientPromise = client.connect();
+    globalAny._mongoClientPromise = client.connect();
   }
-  // @ts-ignore
-  clientPromise = global._mongoClientPromise;
+  clientPromise = globalAny._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
